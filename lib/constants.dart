60d2ea7e73e64ef7f097c630f747ac7342a9473a
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-03-03 23:08:20 -0500

    Ajuste de colores.

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFFF67952);
 const Color bgColor = Color(0xFFFBFBFD);
 const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xFF000000);
 
 const double defaultPadding = 16.0;
 const double defaultBorderRadius = 12.0;
