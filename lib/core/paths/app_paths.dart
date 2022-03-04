c3d0a8bd3a8e90cfa52e84a48784376575942c98
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-03-03 22:26:54 -0500

    pantalla LoginView.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/gui/views/home_view/home_view.dart';
+import 'package:shop_app/gui/views/login_view/login_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
 
   'home': ( _ ) => const HomeView(),
+  'login': ( _ ) => const LoginView()
 
 };
