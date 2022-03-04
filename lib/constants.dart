2aafc4bd98e1ea14a3e2967eff6f5d2011505015
Stiven Gomez <s4.gomez@praxisglobe.net>
2022-03-03 22:43:04 -0500

    archivo constants.

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFF67952);
+const Color bgColor = Color(0xFFFBFBFD);
+const Color whiteColor = Color(0xFFFFFFFF);
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0;
