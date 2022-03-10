8bfd2814abf33663b8cc6d7d012f01d704919cf5
Stiven Gomez <stivenalexander7u7@gmail.com>
2022-03-10 16:58:21 -0500

    Pantalla de registro.

--- lib/gui/views/register_view/register_view.dart
+++ lib/gui/views/register_view/register_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/gui/views/register_view/form_register.dart';
+import 'package:shop_app/gui/widgets/curved_widget.dart';
+import 'package:shop_app/gui/widgets/text_app.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({ Key? key }) : super(key: key);
@@ -13,23 +15,39 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/arrow_left.svg'),
-          iconSize: 35.0,
-          color: greyColor,
-          onPressed: () => Navigator.pop(context),
-        ),
         elevation: 0,
-        backgroundColor: bgColor,
+        backgroundColor: Colors.transparent,
       ),
-      body: _body(),
+      body: SizedBox(
+        height: double.infinity,
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                CurvedWidget(
+                  curvedDistance: 80.0,
+                  curvedHeight: 80.0,
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(top: 100.0, left: 50.0),
+                    height: 300.0,
+                    color: primaryColor,
+                    child: const TextApp(text: 'Registro', fontSize: 40.0),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 300.0, right: 28.0, left: 28.0),
+                  child: const FormLogin(),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
-  Widget _body() {
-    return Container(
-      
-    );
-  }
 }
